74155cec57a0b74586ea68c3e01a3b10b8530ecc
pc <rishi0189pjain@gmail.com>
2022-08-04 23:37:59 +0530

    First spring mvc prog

---
+++ basicwebmvnfirstprog/src/main/java/com/learn/bwebmvc/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.learn.bwebmvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HomeController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mav = null;
+		
+		mav = new ModelAndView();
+		mav.setViewName("home");
+		return mav;
+	}
+
+}
